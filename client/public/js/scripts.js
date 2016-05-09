08a3fa2c0e659ccb94ccf93ddc2e75167968b03f
Carolyn Cochran <cochran.cj@gmail.com>
2016-05-09 17:33:14 -0400

    right after class

--- client/public/js/scripts.js
+++ client/public/js/scripts.js
@@ -1,66 +1,54 @@
-// CC -- PLAYING WITH SEMANTIC UI ELEMENTS
-
-// $(function(){
-//   $('a.item').click(function(){
-//     $('.item').removeClass('active');
-//     $(this).addClass('active');
-//   });
-//   $('.accordion').accordion();
-// });
-
-// $(function(){
-//   $('.ui.checkbox')
-//     .checkbox();
-// }
 
 // SIGNUP FORM VALIDATION
+function formSubmit (){
 
-$('.ui.form')
-  .form({
-    fields: {
-      username: {
-        identifier: 'username',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter a username'
-          }
-        ]
-      },
-      email: {
-        identifier  : 'email',
-        rules: [
-          {
-            type   : 'email',
-            prompt : 'Please enter a valid e-mail'
-          }
-        ]
-      },
-      password: {
-        identifier: 'password',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter a password'
-          },
-          {
-            type   : 'minLength[6]',
-            prompt : 'Your password must be at least {ruleValue} characters'
-          }
-        ]
-      },
-      terms: {
-        identifier: 'terms',
-        rules: [
-          {
-            type   : 'checked',
-            prompt : 'You must agree to the terms and conditions'
-          }
-        ]
+  $('.ui.form')
+    .form({
+      fields: {
+        username: {
+          identifier: 'username',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Please enter a username'
+            }
+          ]
+        },
+        email: {
+          identifier  : 'email',
+          rules: [
+            {
+              type   : 'email',
+              prompt : 'Please enter a valid e-mail'
+            }
+          ]
+        },
+        password: {
+          identifier: 'password',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Please enter a password'
+            },
+            {
+              type   : 'minLength[6]',
+              prompt : 'Your password must be at least {ruleValue} characters'
+            }
+          ]
+        },
+        terms: {
+          identifier: 'terms',
+          rules: [
+            {
+              type   : 'checked',
+              prompt : 'You must agree to the terms and conditions'
+            }
+          ]
+        }
       }
-    }
-  })
-;
+    });
+}
+formSubmit();
 
 
 // PARALLAX FUNCTIONS - CC
@@ -70,18 +58,19 @@ $('.ui.form')
 
     var $height = $(window).height();
     var $scrolled = $(window).scrollTop();
-
 // background
-
     var $background = $('.bg');
     $background.css("top", ($scrolled * -1) + "px");
-    // var $logoBox = $('#big-logo');
-    // $logoBox.css("top", ($scrolled * -1) + "px");
-
-    var $scoreBox = $('.hipscore');
-    $scoreBox.css("top", ($scrolled * 1.5) + "px");
-
-})
+// logo bar thing
+    var $logoBox = $('#big-logo');
+    $logoBox.css("top", ($scrolled * -2) + "px");
+// custom hipscore
+    var $scoreBox = $('#hipscore');
+    $scoreBox.css("top", ($scrolled * -2) + "px");
+// dummy listings + ads
+    var $listingBox = $('#adspace');
+    $listingBox.css("top", ($scrolled * -2) + "px");
+});
 
 // MODAL FUNCTIONS
 function modal(){
@@ -90,29 +79,23 @@ function modal(){
 }
 modal();
 
+// CHECKBOX FUNCTIONS
+function checkbox(){
+  $('.ui.checkbox')
+  .checkbox()
+;
+}
+checkbox();
+
 
 // SOCIAL MEDIA BUTTON JIGGGLE - NOT WORKING
-// function jiggle(){
-//
-// $('.social-buttons.button')
-//   .transition({
-//     animation : 'jiggle',
-//     duration  : 800,
-//     interval  : 200
-//   });
-// };
-// jiggle();
+function jiggle(){
 
-
-// CAROUSEL - INSTEAD OF MODAL FOR QUIZ, ETC., WHAT ABOUT A CAROUSEL?
-// $(document).ready(function(){
-//   $('.owl-carousel').owlCarousel({
-//     animateOut: 'slideOutDown',
-//     animateIn: 'flipInX',
-//     items:1,
-//     margin:30,
-//     stagePadding:30,
-//     smartSpeed:450
-// });
-// });
-// END CAROUSEL
+$('.social-buttons.button')
+  .transition({
+    animation : 'jiggle',
+    duration  : 800,
+    interval  : 200
+  });
+};
+jiggle();
