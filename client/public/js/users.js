9efc88d55df14e1547fbb6801f0703ab45a2d17d
Dev Eggers <eggers.brendan@gmail.com>
2016-05-12 18:22:38 -0400

    commits the final editing thing

--- client/public/js/users.js
+++ client/public/js/users.js
@@ -1,14 +1,14 @@
 var $hipscore = 0;
 
 
-function addScore(){
+function addScore( score ){
   $inputs = $('.checked').find('input');
   for (var i = 0; i < $inputs.length; i++) {
     $numString = $inputs[i].value;
     $num = parseInt($numString);
-    $hipscore += $num;
+    score += $num;
   }
-  return $hipscore;
+  return score;
 }
 
 
@@ -17,7 +17,7 @@ function createUser(){
   userInfo.username = $('#newusername').val();
   userInfo.email = $('#newemail').val();
   userInfo.password = $('#newpassword').val();
-  userInfo.hipscore = addScore();
+  userInfo.hipscore = addScore( $hipscore );
   console.log( userInfo.hipscore );
   $.ajax({
     method: 'post',
@@ -70,16 +70,17 @@ function loginUser(){
     });
   });
 }
-
-function updateThis(){
+function updateScore(){
   var id = $("#userID").text();
-  $("#profile-form").on('click', '#edit-profile-button', function(e){
+  var newScore = 0;
+  $("#preferences-form").on('click', '#save-preferences-button', function(e){
     e.preventDefault();
     var newUsername = $('#profile-form #username').val();
     var newPassword = $('#profile-form #password').val();
     var newEmail = $('#profile-form #email').val();
-    var hipscore = addScore();
-    var payload = JSON.stringify({ username: newUsername, password: newPassword, email: newEmail, hipscore: hipscore });
+    var hipscore = addScore(newScore);
+    console.log(hipscore);
+    var payload = JSON.stringify({username: newUsername,password:newPassword,email:newEmail,hipscore:hipscore});
 
     $.ajax({
       url: '/api/users/'+id,
@@ -91,7 +92,30 @@ function updateThis(){
       }
     });
   });
+}
 
+
+function updateThis(){
+  var id = $("#userID").text();
+  $("#profile-form").on('click', '#edit-profile-button', function(e){
+    e.preventDefault();
+    var newUsername = $('#profile-form #username').val();
+    var newPassword = $('#profile-form #password').val();
+    var newEmail = $('#profile-form #email').val();
+    var hipscore = parseInt($('#userScore').text() );
+    console.log(hipscore);
+    var payload = JSON.stringify({username: newUsername,password:newPassword,email:newEmail,hipscore:hipscore});
+
+    $.ajax({
+      url: '/api/users/'+id,
+      method: 'PUT',
+      contentType: 'application/json',
+      data: payload,
+      success: function(response){
+        window.location.reload();
+      }
+    });
+  });
 }
 
 function logout(){
@@ -110,11 +134,10 @@ $(document).ready(function(){
 
   loginUser();
   updateThis();
+  updateScore();
 
   $('#logout').on('click', function(e){
     e.preventDefault();
     logout();
   });
-
-  
 });
