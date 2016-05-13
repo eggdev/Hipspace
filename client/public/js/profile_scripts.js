4676929ba53caff41d779255b6d41a01953affb5
Blake Zbylut <blakezbylut@gmail.com>
2016-05-12 20:49:02 -0400

    adds comments

--- client/public/js/profile_scripts.js
+++ client/public/js/profile_scripts.js
@@ -1,17 +1,19 @@
 $(function(){
   checkbox();
   movingOwl();
-  raindrop();
-  $('select.dropdown').dropdown();
-  newScore();
+  dropdown();
 })
 
-
+//semantic requirements for the toggles and dropdowns to run
 function checkbox(){
    $('.ui.checkbox').checkbox();
 }
 
+function dropdown(){
+  $('select.dropdown').dropdown();
+}
 
+// this function moves the owl across the bottom of the profile screen
 function movingOwl(){
   var $owl = $("#owl");
   var distance = 1200;
@@ -23,35 +25,4 @@ function movingOwl(){
       distance -= 5;
     }
   }, 80);
-}
-
-function raindrop(){
-  var $drop = $("#raindrop");
-  var distance = 900;
-  setInterval(function(){
-    $drop.css('top', distance + 'px');
-    if(distance < -300) {
-      distance = 900;
-    } else {
-      distance -= 5;
-    }
-  }, 80);
-}
-
-function addScore(){
-  var $hipscore = 0;
-  $inputs = $('.checked').find('input');
-  for (var i = 0; i < $inputs.length; i++) {
-    $numString = $inputs[i].value;
-    $num = parseInt($numString);
-    $hipscore = $hipscore + $num;
-  };
-  $("#score").empty();
-  $("#score").append($hipscore);
-}
-
-function newScore(){
-  $("#save-preferences-button").on('click',function(){
-    addScore();
-  })
 }
